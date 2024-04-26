5d7007a158b737de35717c76f0b4cfab79e9da43
stevenmettler <stevenmettler4@gmail.com>
2024-04-26 16:41:29 -0400

    adding more

--- devotionail/urls.py
+++ devotionail/urls.py
@@ -26,20 +26,59 @@ client = Groq(
 )
 
 
-def hello(request):
+def topic(request):
     chat_completion = client.chat.completions.create(
+        #
+        # Required parameters
+        #
         messages=[
+            # Set an optional system message. This sets the behavior of the
+            # assistant and can be used to provide specific instructions for
+            # how it should behave throughout the conversation.
+            {
+                "role": "system",
+                "content": "you are a Christian. You believe in the Bible as the supreme authority of truth in all things and you submit to the teachings of Jesus Christ. Your one goal is to accept a topic and generate a Bible study with three scriptures as well as three contemplative questions as your output."
+            },
+            # Set a user message for the assistant to respond to.
             {
                 "role": "user",
-                "content": "Explain the importance of low latency LLMs, explain it in the voice of Jon Snow",
+                "content": "Topic: Grace",
             }
         ],
+
+        # The language model which will generate the completion.
         model="llama3-70b-8192",
+
+        #
+        # Optional parameters
+        #
+
+        # Controls randomness: lowering results in less random completions.
+        # As the temperature approaches zero, the model will become deterministic
+        # and repetitive.
+        temperature=0.5,
+
+        # The maximum number of tokens to generate. Requests can use up to
+        # 2048 tokens shared between prompt and completion.
+        max_tokens=1024,
+
+        # Controls diversity via nucleus sampling: 0.5 means half of all
+        # likelihood-weighted options are considered.
+        top_p=1,
+
+        # A stop sequence is a predefined or user-specified text string that
+        # signals an AI to stop generating content, ensuring its responses
+        # remain focused and concise. Examples include punctuation marks and
+        # markers like "[end]".
+        stop=None,
+
+        # If set, partial message deltas will be sent.
+        stream=False,
     )
     return HttpResponse(chat_completion.choices[0].message.content)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", hello, name="hello"),  # ← Added!
+    path("", topic, name="topic"),  # ← Added!
 ]
