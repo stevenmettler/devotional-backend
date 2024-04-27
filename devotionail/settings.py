b83509adc863a068822da09fab8b0be48e49f198
stevenmettler <stevenmettler4@gmail.com>
2024-04-27 19:28:11 -0400

    cors again

--- devotionail/settings.py
+++ devotionail/settings.py
@@ -55,6 +55,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
+
 ROOT_URLCONF = 'devotionail.urls'
 
 TEMPLATES = [
