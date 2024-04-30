5e1b0613f3c3512c70321244141047a1d28665e1
stevenmettler <stevenmettler4@gmail.com>
2024-04-30 15:57:41 -0400

    added slash

--- devotionail/urls.py
+++ devotionail/urls.py
@@ -14,17 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from groq import Groq
 from devotionail import settings
+
 # devotionail/urls.py
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-client = Groq(
-    api_key=settings.GROQ_API_KEY
-)
+client = Groq(api_key=settings.GROQ_API_KEY)
 
 
 @csrf_exempt
@@ -39,41 +39,34 @@ def topic(request, topic):
             # how it should behave throughout the conversation.
             {
                 "role": "system",
-                "content": "you are a Christian. You believe in the Bible as the supreme authority of truth in all things and you submit to the teachings of Jesus Christ. Your one goal is to accept a topic and generate a Bible study with three scriptures as well as three contemplative questions as your output. I will ask you to give me personal Bible studies on a specific topic, and you will return concise studies with 3 or so scriptures and 3 contemplative questions at the end that aims to help a user get a closer relationship with God. You believe that faith, repentance, and baptism are necessary for salvation. The output should be in JSON format. Please do not output anything beside the JSON formatted response. Please use the JSON template below to format the response as well as the random one-word topic of your choice. The Json template should include valid JSON keys that are strings and values of the keys that are also strings. The template should look like this: **Json template:** {  study: [    {      scripture: Scripture1,      text: Text1    },    {      scripture: Scripture2,      text: Text2    },    {      scripture: Scripture3,      text: Text3    }  ],  questions: [    Contemplative Question 1,    Contemplative Question 2,    Contemplative Question 3  ]}"
+                "content": "you are a Christian. You believe in the Bible as the supreme authority of truth in all things and you submit to the teachings of Jesus Christ. Your one goal is to accept a topic and generate a Bible study with three scriptures as well as three contemplative questions as your output. I will ask you to give me personal Bible studies on a specific topic, and you will return concise studies with 3 or so scriptures and 3 contemplative questions at the end that aims to help a user get a closer relationship with God. You believe that faith, repentance, and baptism are necessary for salvation. The output should be in JSON format. Please do not output anything beside the JSON formatted response. Please use the JSON template below to format the response as well as the random one-word topic of your choice. The Json template should include valid JSON keys that are strings and values of the keys that are also strings. The template should look like this: **Json template:** {  study: [    {      scripture: Scripture1,      text: Text1    },    {      scripture: Scripture2,      text: Text2    },    {      scripture: Scripture3,      text: Text3    }  ],  questions: [    Contemplative Question 1,    Contemplative Question 2,    Contemplative Question 3  ]}",
             },
             # Set a user message for the assistant to respond to.
             {
                 "role": "user",
-                "content": f"The topic is {topic}. Give me a study with valid json. Please do not output anything beside a valid json."
-            }
+                "content": f"The topic is {topic}. Give me a study with valid json. Please do not output anything beside a valid json.",
+            },
         ],
-
         # The language model which will generate the completion.
         model="llama3-70b-8192",
-
         #
         # Optional parameters
         #
-
         # Controls randomness: lowering results in less random completions.
         # As the temperature approaches zero, the model will become deterministic
         # and repetitive.
         temperature=0.5,
-
         # The maximum number of tokens to generate. Requests can use up to
         # 2048 tokens shared between prompt and completion.
         max_tokens=1024,
-
         # Controls diversity via nucleus sampling: 0.5 means half of all
         # likelihood-weighted options are considered.
         top_p=1,
-
         # A stop sequence is a predefined or user-specified text string that
         # signals an AI to stop generating content, ensuring its responses
         # remain focused and concise. Examples include punctuation marks and
         # markers like "[end]".
         stop=None,
-
         # If set, partial message deltas will be sent.
         stream=False,
     )
@@ -92,41 +85,34 @@ def random(request):
             # how it should behave throughout the conversation.
             {
                 "role": "system",
-                "content": "you are a Christian. You believe in the Bible as the supreme authority of truth in all things and you submit to the teachings of Jesus Christ. Your one goal is to accept a topic and generate a Bible study with three scriptures as well as three contemplative questions as your output. I will ask you to give me personal Bible studies on a specific topic, and you will return concise studies with 3 or so scriptures and 3 contemplative questions at the end that aims to help a user get a closer relationship with God. You believe that faith, repentance, and baptism are necessary for salvation. The output should be in JSON format. Please do not output anything beside the JSON formatted response. Please use the JSON template below to format the response as well as the random one-word topic of your choice. The Json template should include valid JSON keys that are strings and values of the keys that are also strings. The template should look like this: **Json template:** {  study: [    {      scripture: Scripture1,      text: Text1    },    {      scripture: Scripture2,      text: Text2    },    {      scripture: Scripture3,      text: Text3    }  ],  questions: [    Contemplative Question 1,    Contemplative Question 2,    Contemplative Question 3  ]}"
+                "content": "you are a Christian. You believe in the Bible as the supreme authority of truth in all things and you submit to the teachings of Jesus Christ. Your one goal is to accept a topic and generate a Bible study with three scriptures as well as three contemplative questions as your output. I will ask you to give me personal Bible studies on a specific topic, and you will return concise studies with 3 or so scriptures and 3 contemplative questions at the end that aims to help a user get a closer relationship with God. You believe that faith, repentance, and baptism are necessary for salvation. The output should be in JSON format. Please do not output anything beside the JSON formatted response. Please use the JSON template below to format the response as well as the random one-word topic of your choice. The Json template should include valid JSON keys that are strings and values of the keys that are also strings. The template should look like this: **Json template:** {  study: [    {      scripture: Scripture1,      text: Text1    },    {      scripture: Scripture2,      text: Text2    },    {      scripture: Scripture3,      text: Text3    }  ],  questions: [    Contemplative Question 1,    Contemplative Question 2,    Contemplative Question 3  ]}",
             },
             # Set a user message for the assistant to respond to.
             {
                 "role": "user",
                 "content": f"Give me a study with valid json. Please do not output anything beside a valid json.",
-            }
+            },
         ],
-
         # The language model which will generate the completion.
         model="llama3-70b-8192",
-
         #
         # Optional parameters
         #
-
         # Controls randomness: lowering results in less random completions.
         # As the temperature approaches zero, the model will become deterministic
         # and repetitive.
         temperature=0.5,
-
         # The maximum number of tokens to generate. Requests can use up to
         # 2048 tokens shared between prompt and completion.
         max_tokens=1024,
-
         # Controls diversity via nucleus sampling: 0.5 means half of all
         # likelihood-weighted options are considered.
         top_p=1,
-
         # A stop sequence is a predefined or user-specified text string that
         # signals an AI to stop generating content, ensuring its responses
         # remain focused and concise. Examples include punctuation marks and
         # markers like "[end]".
         stop=None,
-
         # If set, partial message deltas will be sent.
         stream=False,
     )
@@ -135,6 +121,6 @@ def random(request):
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("topic/<str:topic>", topic, name="topic"),
+    path("topic/<str:topic>/", topic, name="topic"),
     path("random/", random, name="random"),
 ]
